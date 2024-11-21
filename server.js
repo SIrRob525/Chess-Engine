92cfb778eb2c9440c0bf9c44b4d8217e54cf198f
Boris <i.boris05@gmail.com>
2024-11-21 10:15:34 +0800

    created browser game

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 8000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "game"), {
+    setHeaders: (res, path) => {
+        res.set("Cross-Origin-Embedder-Policy", "require-corp");
+        res.set("Cross-Origin-Opener-Policy", "same-origin");
+    }
+}));
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
